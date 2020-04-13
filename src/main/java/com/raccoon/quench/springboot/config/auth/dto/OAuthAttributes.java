347f4b85a92f86f9e329839432d9157819050b2c
raccoonjeong <rana3782@gmail.com>
2020-04-13 10:06:25 +0900

    Chapter05 OAuth2.0 Google, Naver 적용 완료

--- src/main/java/com/raccoon/quench/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/raccoon/quench/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,6 +28,9 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registractionId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
+            if("naver".equals(registractionId)) {
+                return ofNaver("id", attributes);
+            }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -40,6 +43,20 @@ public class OAuthAttributes {
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName,
+                                           Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+
     }
 
     public User toEntity() {
