11986054d1489c54a074923ca381b836459c7499
raccoonjeong <rana3782@gmail.com>
2020-05-14 20:05:12 +0900

    테스트 수정

--- src/test/java/com/raccoon/quench/springboot/web/IndexControllerTest.java
+++ src/test/java/com/raccoon/quench/springboot/web/IndexControllerTest.java
@@ -22,7 +22,7 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         // then
-        assertThat(body).contains("<h1>");
+        assertThat(body).contains("<div>");
     }
 
 }
