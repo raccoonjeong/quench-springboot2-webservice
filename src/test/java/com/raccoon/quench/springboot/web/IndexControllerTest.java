7b183d7f635708e26f68f562e0ee9c82b4062b4b
raccoonjeong <rana3782@gmail.com>
2020-05-14 20:34:03 +0900

    테스트 주석처리

--- src/test/java/com/raccoon/quench/springboot/web/IndexControllerTest.java
+++ src/test/java/com/raccoon/quench/springboot/web/IndexControllerTest.java
@@ -1,20 +1,20 @@
 package com.raccoon.quench.springboot.web;
 
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexControllerTest {
-    @Autowired
-    private TestRestTemplate restTemplate;
+//    @Autowired
+//    private TestRestTemplate restTemplate;
 
 //    @Test
 //    public void 메인페이지_로딩() {
