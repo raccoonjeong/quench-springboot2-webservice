3be163410b1ab43db49e73959b7874345419cd85
raccoonjeong <rana3782@gmail.com>
2020-05-27 20:41:16 +0900

    Travis CI와 AWS S3, CodeDeploy 연동하기 - deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,4 +33,4 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 
-nohup java -jar -Dspring.profiles.active=real -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml $REPOSITORY/$JAR_NAME 2>&1 &
+nohup java -jar -Dspring.profiles.active=real -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
