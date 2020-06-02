7c5955eccecd512830690f19451a3c5241301ac0
raccoonjeong <rana3782@gmail.com>
2020-06-02 20:29:01 +0900

    무중단 배포 스크립트 만들기(테스트수정)

--- src/test/java/com/raccoon/quench/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/raccoon/quench/springboot/domain/posts/PostsRepositoryTest.java
@@ -12,8 +12,8 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class PostsRepositoryTest {
 
 //    @Autowired
